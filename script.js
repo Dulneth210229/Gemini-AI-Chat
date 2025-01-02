951f0f0135d21c164a910f55121b9119fa5246f7
Dulneth210229 <dulneth.sa@gmail.com>
2025-01-02 23:27:23 +0530

    Select all the elements that are required

--- script.js
+++ script.js
@@ -3,4 +3,9 @@
 //URL=https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyCObe2NvfXh2oarwaaINS8jhgcO_CWnNGw"
 
 //make sure all the assets are loaded
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const chatForm = document.getElementById("chatForm");
+  const userInput = document.getElementById("userInput");
+  const chatMessages = document.getElementById("chatMessages");
+  const sendButton = document.getElementById("sendButton");
+});
