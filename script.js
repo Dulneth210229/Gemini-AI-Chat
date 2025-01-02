13ab394fff9072e506c9127c5ee91d0c84632ef8
Dulneth210229 <dulneth.sa@gmail.com>
2025-01-02 23:25:31 +0530

    make sure all the assets are loaded

---
+++ script.js
@@ -0,0 +1,6 @@
+//apiKey = AIzaSyCObe2NvfXh2oarwaaINS8jhgcO_CWnNGw
+
+//URL=https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyCObe2NvfXh2oarwaaINS8jhgcO_CWnNGw"
+
+//make sure all the assets are loaded
+document.addEventListener("DOMContentLoaded", () => {});
